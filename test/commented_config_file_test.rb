1dc952745b222a53cb282358a3eccd841213ed24
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2017-11-25 21:38:22 +0100

    Added format tests

--- test/commented_config_file_test.rb
+++ test/commented_config_file_test.rb
@@ -80,7 +80,7 @@ describe CommentedConfigFile do
     end
   end
 
-  context "Parser" do
+  context "Low-level parser" do
     describe "#comment_line?" do
       subject { described_class.new }
 
@@ -139,7 +139,7 @@ describe CommentedConfigFile do
 
     describe "#empty_line?" do
       subject { described_class.new }
-      
+
       it "Detects a completely empty line" do
         expect(subject.empty_line?("")).to eq true
       end
@@ -186,6 +186,122 @@ describe CommentedConfigFile do
 
       it "Handles empty lines well" do
         expect(subject.split_off_comment("")).to eq ["", nil]
+      end
+    end
+  end
+
+  context "High-level parser" do
+    describe "#parse" do
+      context "Demo /etc/fstab with header and footer comments" do
+        before(:all) do
+          @file = described_class.new
+          @file.read("data/demo-fstab")
+        end
+        subject { @file }
+
+        it "Has the correct header comments" do
+          header = subject.header_comments
+          expect(header.size).to eq 15
+          expect(header[0]).to match /static file system information/
+          expect(header[-2]).to match /mount point.*type.*dump.*pass/
+          expect(header[-1]).to match /^\s*$/
+        end
+
+        it "Has the correct footer comments" do
+          footer = subject.footer_comments
+          expect(footer.size).to eq 1
+          expect(footer[0]).to match /^\s*$/
+        end
+
+        it "Has the correct number of entries" do
+          expect(subject.entries.size).to eq 9
+        end
+
+        it "The first entry is correct" do
+          entry = subject.entries.first
+          expect(entry.content).to match /by-label\/swap\s+none\s+swap/
+          expect(entry.comment_before.size).to eq 1
+          expect(entry.comment_before.first).to match /Linux disk/
+        end
+
+        it "The root filesystem entry is correct" do
+          entry = subject.entries[2]
+          expect(entry.content).to match /Ubuntu.*ext4.*errors=remount-ro/
+          expect(entry.comment_before?).to eq false
+          expect(entry.line_comment?).to eq false
+        end
+
+        it "The last entry is correct" do
+          entry = subject.entries.last
+          expect(entry.content).to match /fritz.box.fritz.nas.*cifs.*forcegid/
+          expect(entry.comment_before.first).to match /^\s*$/
+        end
+      end
+
+      context "Demo /etc/fstab without header and footer comments" do
+        before(:all) do
+          @file = described_class.new
+          @file.read("data/demo-fstab-no-header")
+        end
+        subject { @file }
+
+        it "Does not have header comments" do
+          expect(subject.header_comments?).to eq false
+        end
+
+        it "Does not have footer comments" do
+          expect(subject.footer_comments?).to eq false
+        end
+
+        it "Has the correct number of entries" do
+          expect(subject.entries.size).to eq 9
+        end
+
+        it "The first entry is correct" do
+          entry = subject.entries.first
+          expect(entry.content).to match /by-label\/swap\s+none\s+swap/
+          expect(entry.comment_before?).to eq false
+        end
+
+        it "The root filesystem entry is correct" do
+          entry = subject.entries[2]
+          expect(entry.content).to match /Ubuntu.*ext4.*errors=remount-ro/
+          expect(entry.comment_before?).to eq false
+          expect(entry.line_comment?).to eq false
+        end
+
+        it "The last entry is correct" do
+          entry = subject.entries.last
+          expect(entry.content).to match /fritz.box.fritz.nas.*cifs.*forcegid/
+          expect(entry.comment_before.first).to match /^\s*$/
+        end
+      end
+    end
+  end
+
+  context "Formatter" do
+    describe("#format_lines") do
+      def read_twice(filename)
+        orig = File.read(filename).chomp
+        file = described_class.new
+        file.read(filename)
+        formatted = file.to_s
+        [orig, formatted]
+      end
+
+      it "reproduces exactly the original format with header and footer" do
+        orig, formatted = read_twice("data/demo-fstab")
+        expect(formatted).to eq orig
+      end
+
+      it "reproduces exactly the original format without header or footer" do
+        orig, formatted = read_twice("data/demo-fstab-no-header")
+        expect(formatted).to eq orig
+      end
+
+      it "reproduces exactly the original format for demo-sudoers" do
+        orig, formatted = read_twice("data/demo-sudoers")
+        expect(formatted).to eq orig
       end
     end
   end
