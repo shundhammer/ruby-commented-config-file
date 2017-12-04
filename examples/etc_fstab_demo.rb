d54af86cfcafdb62dd93a769d0d5b75676597bd5
Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
2017-12-04 19:14:17 +0100

    Ported EtcFstab class from C++

---
+++ examples/etc_fstab_demo.rb
@@ -0,0 +1,64 @@
+#!/usr/bin/env ruby
+#
+# Demo for the EtcFstab class
+#
+# (c) 2017 Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
+#
+# License: GPL V2
+#
+
+SRC_PATH = File.expand_path("../../src", __FILE__)
+$LOAD_PATH << SRC_PATH
+
+require_relative "../src/etc_fstab"
+
+def usage
+  $stderr.puts("\nUsage: etc_fstab_demo <infile-name>\n\n")
+  exit(1)
+end
+
+def dump_entry(entry)
+  puts("<Entry>")
+  puts("  device:        #{entry.device}")
+  puts("  mount point:   #{entry.mount_point}")
+  puts("  fs type:       #{entry.fs_type}")
+  puts("  mount options: #{entry.mount_opts}")
+  puts("  dump pass:     #{entry.dump_pass}")
+  puts("  fsck pass:     #{entry.fsck_pass}")
+  puts("</Entry>")
+end
+
+def report_mount_order_problems(fstab)
+  if fstab.check_mount_order
+    puts("Mount order okay.")
+    return
+  end
+
+  puts
+  puts("*** MOUNT ORDER ERROR! ***")
+  problem_index = 0
+  while true
+    problem_index = fstab.next_mount_order_problem(problem_index)
+    break if problem_index == -1
+    entry = fstab.entries[problem_index]
+    puts("Mount point #{entry.mount_point} is out of sequence!")
+  end
+  fix_mount_order(fstab)
+end
+
+def fix_mount_order(fstab_old)
+  puts("\nSuggested sequence (stripped header and footer comments):\n\n")
+  fstab = fstab_old.dup
+  fstab.fix_mount_order
+  fstab.header_comments = nil
+  fstab.footer_comments = nil
+  # fstab.entries.each { |entry| entry.comment_before = nil }
+  fstab.write("/dev/stdout")
+end
+
+usage unless ARGV.size == 1
+
+filename = ARGV[0]
+fstab = EtcFstab.new(filename)
+fstab.entries.each { |e| dump_entry(e) }
+report_mount_order_problems(fstab)
---
+++ src/etc_fstab.rb
@@ -0,0 +1,389 @@
+#!/usr/bin/env ruby
+#
+# CommentedConfigFile class
+#
+# (c) 2017 Stefan Hundhammer <Stefan.Hundhammer@gmx.de>
+#
+# License: GPL V2
+#
+
+require "column_config_file"
+
+# Class to handle /etc/fstab of a Linux/Unix system.
+#
+# This includes parsing and formatting, accessing and modifying entries,
+# maintaining the correct order between them (since they might have
+# dependencies between mount points) and keeping comments in the file intact
+# (see the CommentedConfigFile and ColumnConfigFile base classes).
+#
+# Use the "entries" member variable (inherited from CommentedConfigFile) to
+# access the entries.
+#
+# To add a new entry, it is strongly advised to use add_entry from this class
+# rather than just appending an entry via the inherited "entries" member:
+# add_entry takes care of the correct order of mount points in case there are
+# depencencies.
+#
+# For example, an entry for /var/lib/myapp should appear AFTER the entry for
+# /var/lib, otherwise /var/lib (if mounted after /var/lib/myapp) would shadow
+# the already mounted /var/lib/myapp which is typically not desired.
+#
+# Important / useful inherited methods:
+# read, write, parse, format
+#
+class EtcFstab < ColumnConfigFile
+  # The usual name of that file
+  ETC_FSTAB = "/etc/fstab".freeze
+
+  # Constructor.
+  #
+  # @param filename [String] File to read if specified.
+  #
+  def initialize(filename = nil)
+    super()
+    @max_column_widths = [45, 25, 7, 30, 1, 1]
+    @pad_columns = true
+    read(filename) unless filename.nil?
+  end
+
+  # Add an entry. The entry can be created with EtcFstab::Entry.create_entry or
+  # with plain EtcFstab::Entry.new; it will always be reparented to this
+  # EtcFstab instance.
+  #
+  # It is strongly advised to use this add_entry method rather than just
+  # appending an entry via the inherited "entries" member: add_entry takes care
+  # of the correct order of mount points in case there are depencencies:
+  #
+  # For example, an entry for /var/lib/myapp should appear AFTER the entry for
+  # /var/lib, otherwise /var/lib (if mounted after /var/lib/myapp) would shadow
+  # the already mounted /var/lib/myapp which is typically not desired.
+  #
+  # @param entry [EtcFstab::Entry]
+  #
+  def add_entry(entry)
+    entry.parent = self
+    index = find_sort_index(entry)
+    @entries.insert(index, entry)
+  end
+
+  # Check the mount order of all the entries, i.e. if all entries are listed
+  # after any mount points they depend on. Call fix_mount_order to fix the
+  # problem.
+  #
+  # For example, if an entry for /var/lib/myapp appears before the entry for
+  # /var/lib, the /var/lib would shadow /var/lib/myapp, so this method would
+  # return 'false'.
+  #
+  # @return [Boolean] 'true' if okay, 'false' if there are mount order problems.
+  #
+  def check_mount_order
+    next_mount_order_problem == -1
+  end
+
+  # Fix any mount order problems: Make sure the entries are listed in the
+  # correct order.
+  #
+  # For example, if an entry for /var/lib/myapp appears before the entry for
+  # /var/lib, the /var/lib would shadow /var/lib/myapp. This method fixes this.
+  #
+  def fix_mount_order
+    start_index = 0
+    while start_index < @entries.size
+      problem_index = next_mount_order_problem(start_index)
+      return if problem_index == -1 # No more problem -> we are finished.
+
+      # Take this entry out of the entries and put it back at the correct
+      # place.
+      entry = @entries.delete_at(problem_index)
+      add_entry(entry)
+
+      # By definition, the entries vector is now fixed up to this index, so
+      # continue fixing at the next index.
+      start_index = problem_index + 1
+
+      # There is one pathological case, though:
+      #
+      # When two or more entries have the same mount point (which is illegal,
+      # but somebody might write such an fstab manuallly), there is no correct
+      # mount order; this algorithm would get into an endless loop if we now
+      # checked the same index again. But by just proceeding with the next one
+      # (and silently assuming that the one we just changed is well and truly
+      # fixed), we can avoid that endless loop.
+      #
+      # The fstab is of course still incorrect, but there is nothing we can do
+      # about that at this point.
+    end
+  end
+
+  # Find the the entry index of the next mount order problem starting from
+  # 'start_index' or -1 if there is no more.
+  #
+  # @param start_index [Fixnum]
+  #
+  # @return [Fixnum] Next problematic entry index or -1 if no more problems
+  #
+  def next_mount_order_problem(start_index = 0)
+    @entries.drop(start_index).each_with_index do |entry, index|
+      sort_index = find_sort_index(entry)
+      next if sort_index == -1
+      return index if sort_index < index
+    end
+    -1
+  end
+
+  protected
+
+  # Find the correct index for an entry if it depends on any other entry or -1
+  # if no other entry depends on this one, i.e. it can safely added to the end
+  # of the list.
+  #
+  # @param new_entry [EtcFstab::Entry]
+  #
+  # @return [Fixnum] correct index or -1 if there is no dependency
+  #
+  def find_sort_index(new_entry)
+    mount_point = new_entry.mount_point
+    @entries.each_with_index do |entry, index|
+      next if entry === new_entry
+      return index if entry.mount_point.start_with?(mount_point)
+    end
+    -1
+  end
+
+  public
+
+  # Get the "mount by" type of a device entry in /etc/fstab.
+  # See also EtcFstab::Entry.get_mount_by.
+  #
+  # @param device [String] device field or complete entry line
+  # @return [Symbol] One of :uuid, :label, :id, :path, :device
+  #
+  def self.get_mount_by(device)
+    case device
+    when /^UUID=/, %r{^/dev/disk/by-uuid/}
+      :uuid
+    when /^LABEL=/, %r{^/dev/disk/by-label/}
+      :label
+    when %r{^/dev/disk/by-id/}
+      :id
+    when %r{^/dev/disk/by-path/}
+      :path
+    else
+      :device
+    end
+  end
+
+  # Encode an fstab entry. It may sound surprising, but the file format
+  # actually allows space characters in certain places, such as the mount
+  # point; the reasoning is that a space character is a permitted character in
+  # a directory name, so there has to be a way to specify such a path without
+  # breaking the file format. According to "man fstab", this is done by
+  # encoding the space character in octal, i.e. as \040. This is the function
+  # to do it.
+  #
+  # This class and the corresponding entry class handles this transparently, so
+  # this should never be necessary to use from the outside.
+  #
+  # @param unencoded [String] String with possible space characters
+  # @return [String] String with space characters replaced by \040
+  #
+  def self.fstab_encode(unencoded)
+    unencoded.gsub(" ", "\\040")
+  end
+
+  # Decode an fstab entry. This is the inverse operation to fstab_encode.
+  #
+  # @param encoded [String] String with possible \040 sequences
+  # @return [String] String with \040 replaced by a space character each
+  #
+  def self.fstab_decode(encoded)
+    encoded.gsub("\\040", " ")
+  end
+
+  # Create a new entry.
+  #
+  # Reimplemented from CommentedConfigFile.
+  #
+  # @return [ColumnConfigFile::Entry] new entry
+  #
+  def create_entry
+    EtcFstab::Entry.new(self)
+  end
+
+  #
+  #----------------------------------------------------------------------
+  #
+  #
+  # Entry class for /etc/fstab. This gives each field it semantics rather than
+  # just being numbered columns like in the ColumnConfigFile superclass.
+  #
+  class Entry < ColumnConfigFile::Entry
+    # The columns of each EtcFstab entry; see also "man fstab".
+    #
+    # @return [String] The device that is mounted, including prefixes like
+    # "UUID=" or "LABEL=".
+    attr_accessor :device
+
+    # @return [String] The mount point where the device is mounted.
+    #
+    # This value may contain space characters (because space is a permitted
+    # character in directory names). In the file, those are escaped with
+    # '\040'. This escaping is handled by the parse and format methods of this
+    # class, so don't attempty to do it again when setting or getting this
+    # value.
+    attr_accessor :mount_point
+
+    # @return [String] The filesystem type as specified in /etc/fstab,
+    # i.e. lowercase; typically one of "ext2", "ext3", "btrfs", "xfs", "nfs",
+    # "vfat" etc.
+    attr_accessor :fs_type
+
+    # @return [Array<String>] The mount options, split up in their individual
+    # comma-separated parts. This does not ever contain "default"; in that
+    # case, the array is empty. The parser and formatter take care of removing
+    # or adding "default" if necessary (i.e., when the mount options are
+    # empty).
+    attr_accessor :mount_opts
+
+    # @return [Fixnum] This field is pretty much obsolete; it was there for the
+    # sake of the long obsolete Unix "dump" command, a very old backup
+    # tool. This field is (almost?) always 0 nowadays.
+    attr_accessor :dump_pass
+
+    # @return [Fixnum] The filesystem check pass. This is typically 1 for the
+    # root filesystem, 2 for most others and 0 if no filesystem check should
+    # ever be performed on this device (typically for networked devices such as
+    # NFS or CIFS (Samba)).
+    attr_accessor :fsck_pass
+
+    # Constructor.
+    #
+    # @param parent [EtcFstab]
+    #
+    def initialize(parent)
+      super
+      @device = nil
+      @mount_point = nil
+      @fs_type = nil
+      @mount_opts = []
+      @dump_pass = 0
+      @fsck_pass = 0
+    end
+
+    # Parse a content line. This expects any line comment and the newline to be
+    # stripped off already.
+    #
+    # Reimplemented from ColumnConfigFile::Entry.
+    #
+    # @param line [String] content line without any line comment
+    # @param line_no [Fixnum] line number for error reporting
+    #
+    # @return [Boolean] true if success, false if error
+    #
+    # @raise [EtcFstab.ParseError] Incorrect file format
+    #
+    def parse(line, line_no = -1)
+      super
+      if @columns.size != 6
+        raise EtcFstab.ParseError, "Wrong number of columns in line #{line_no}"
+      end
+      decoded_col = @columns.map { |col| EtcFstab.fstab_decode(col) }
+      @device, @mount_point, @fs_type, opt, @dump_pass, @fsck_pass = decoded_col
+      parse_mount_opts(opt)
+      @dump_pass = @dump_pass.to_i
+      @fsck_pass = @fsck_pass.to_i
+      true # success
+    end
+
+    # Populate the columns: Fill the columns with values from the other fields.
+    #
+    # This is called just prior to calculating the column widths and formatting
+    # the columns.
+    #
+    # Reimplemented from ColumnConfigFile::Entry.
+    #
+    def populate_columns
+      # Strictly speaking, space characters are only permitted in the
+      # mount_point column according to "man fstab". But better be safe than
+      # sorry; there might be a space character also in a device label or in
+      # the mount options, and if the kernel / the mount command / systemd can
+      # handle it, we want to support that, too; so let's simply encode all the
+      # fields.
+      #
+      # On the downside, this does not let us strip all the other fields
+      # because we might strip away a space character that was intentional; so
+      # it's the caller's responsibility to make sure that there are not
+      # unintended space characters in any of the fields.
+
+      @columns =
+        [@device,
+         @mount_point,
+         @fs_type,
+         format_mount_opts,
+         @dump_pass.to_s,
+         @fsck_pass.to_s].map { |col| EtcFstab.fstab_encode(col) }
+    end
+
+    # Get the "mount_by" type of this entry. See EtcFstab::get_mount_by.
+    #
+    # @return [Symbol]
+    #
+    def get_mount_by
+      EtcFstab.get_mount_by(@device)
+    end
+
+    protected
+
+    # Parse a mount options field and store the result in @mount_opts.
+    #
+    # This removes any occurrence of "defaults" (which really only makes sense
+    # if the field would otherwise be empty which is not permitted by the file
+    # format).
+    #
+    # @param opts [String] mount options field
+    #
+    def parse_mount_opts(opts)
+      if opts.nil?
+        @mount_opts = []
+        return
+      end
+      @mount_opts = opts.split(/,/)
+      @mount_opts.delete_if { |opt| opt == "defaults" }
+    end
+
+    # Format the mount options from @mount_opts. If they are empty (and only
+    # then) this returns "defaults" to make sure the file format is still
+    # intact.
+    #
+    # @return [String] Formatted mount options
+    #
+    def format_mount_opts
+      return "defaults" if @mount_opts.empty?
+      @mount_opts.join(",")
+    end
+
+    # Return the column delimiter used for input (parsing).
+    #
+    # Reimplemented from ColumnConfigFile::Entry.
+    #
+    # @return [Regexp] delimiter
+    #
+    def input_delimiter
+      return ColumnConfigFile::DEFAULT_INPUT_DELIMITER
+    end
+
+    # Return the column delimiter used for output, usually two blanks.
+    #
+    # Reimplemented from ColumnConfigFile::Entry.
+    #
+    # @return [String] delimiter
+    #
+    def output_delimiter
+      return ColumnConfigFile::DEFAULT_OUTPUT_DELIMITER
+    end
+  end
+
+  # Error class for parsing
+  class ParseError < RuntimeError
+  end
+end
